f5d625ca2da556e4928c8b8e87fc9706adace383
Luís Cláudio Ribeiro Junior <luis.claudio.jr@hotmail.com>
2019-12-05 01:07:06 -0300

    pooling completo

---
+++ src/Projeto.Data/Util/HibernateUtil.cs
@@ -0,0 +1,31 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using Projeto.Data.Entities;
+using Projeto.Data.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Data.Util
+{
+    public class HibernateUtil
+    {
+        private static ISessionFactory sessionFactory;
+
+        public static ISessionFactory SessionFactory(string connectionString)
+        {
+            if (sessionFactory == null)
+            {
+                sessionFactory = Fluently.Configure()
+                    .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString))
+                    .Mappings(m => m.FluentMappings.Add<AvaliacaoAtendimentoMap>())
+                    .ExposeConfiguration(cfg => new SchemaExport(cfg).Execute(true, true, false))
+                    .BuildSessionFactory();
+            }
+
+            return sessionFactory;
+        }
+    }
+}
